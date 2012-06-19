193fdf1c5874f476e09e514493d1c6f2645fa094
nguyenh <>
2012-06-19 20:55:50 +0000

    *** empty log message ***

--- TriggerFilter/python/triggerFilter_cfi.py
+++ TriggerFilter/python/triggerFilter_cfi.py
@@ -2,14 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 # Bit Plotting
 pfht350PassPrescaleFilter = cms.EDFilter("TriggerFilter",
-                             #directory = cms.untracked.string('myDirectory'),
-                             #histLabel = cms.untracked.string('myHistLabel'),
-                             #label = cms.string('myLabel'),
-                             #out = cms.untracked.string('dqm.root'),
                              HLTPaths = cms.vstring('HLT_PFHT350_v[0-9]'),
                              filterTypes = cms.untracked.vstring( "HLTPrescaler" ),
-                             #denominatorWild = cms.untracked.string(''),
-                             #denominator = cms.untracked.string(''),
-                             #eventSetupPathsKey = cms.untracked.string(''),
                              TriggerResultsTag = cms.InputTag('TriggerResults','','HLT')
 )
